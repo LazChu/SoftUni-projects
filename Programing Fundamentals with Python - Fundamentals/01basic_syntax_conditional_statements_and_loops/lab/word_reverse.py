cefd479f94e015a1d9f42505acbcc61b88d495ec
Lazarin Hristov <106109959+LazChu@users.noreply.github.com>
2022-06-04 06:16:39 +0300

    Delete Programing Fundamentals with Python - Fundamentals directory

--- Programing Fundamentals with Python - Fundamentals/01basic_syntax_conditional_statements_and_loops/lab/word_reverse.py
+++
@@ -1,9 +0,0 @@
-# word = input()
-# reverse = ''
-# for w in range(len(word) -1, -1 ,-1):
-#     reverse  += word[w]
-# print(reverse)
-
-word = input()
-
-print(word[:: -1])
--- Programing Fundamentals with Python - Fundamentals/02data_types_and_variables/lab/pounds_to_dollar.py
+++
@@ -1,3 +0,0 @@
-british_pounds = int(input())
-dollars = british_pounds * 1.31
-print(f'{dollars:.3f}')
